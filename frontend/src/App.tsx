2754b5d3ad51a8459216f1a4f33fda2515bcf070
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2025-10-22 21:57:27 +0200

    Add form state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
 
 function App() {
+  const [temperature, setTemperature] = useState("");
+  const [heartRate, setHeartRate] = useState("");
+  const [respiratoryRate, setRespiratoryRate] = useState("");
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+  };
+
+  const handleReset = () => {
+    setTemperature("");
+    setHeartRate("");
+    setRespiratoryRate("");
   };
 
   return (
@@ -15,16 +26,32 @@ function App() {
           id="body-temp"
           label="Body temperature"
           subLabel="Degrees celcius"
+          value={temperature}
+          onChange={(e) => setTemperature(e.target.value)}
+          type="number"
+          inputMode="numeric"
         />
-        <Input id="heart-rate" label="Heartrate" subLabel="Beats per minute" />
+        <Input
+          id="heart-rate"
+          label="Heartrate"
+          subLabel="Beats per minute"
+          value={heartRate}
+          onChange={(e) => setHeartRate(e.target.value)}
+          type="number"
+          inputMode="numeric"
+        />
         <Input
           id="respiratory-rate"
           label="Respiratory rate"
           subLabel="Breaths per minute"
+          value={respiratoryRate}
+          onChange={(e) => setRespiratoryRate(e.target.value)}
+          type="number"
+          inputMode="numeric"
         />
         <div className="actions">
           <Button>Calculate NEWS score</Button>
-          <Button type="reset" variant="secondary">
+          <Button type="reset" variant="secondary" onClick={handleReset}>
             Reset form
           </Button>
         </div>
