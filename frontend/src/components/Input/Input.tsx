44b61a298831a0561e0a5a7d9d907f7d1c4e50f9
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2025-10-22 21:21:19 +0200

    Style input component

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -1,3 +1,4 @@
+import "./Input.css";
 import { useId } from "react";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -10,15 +11,14 @@ export function Input({ label, subLabel, id, ...delegated }: Props) {
   const generatedId = useId();
   const appliedId = id ?? generatedId;
   return (
-    <label htmlFor={appliedId}>
-      {label}
-      {subLabel && (
-        <>
-          <br />
-          <span>{subLabel}</span>
-        </>
-      )}
-      <input {...delegated} id={appliedId} />
-    </label>
+    <>
+      <div>
+        <label htmlFor={appliedId}>
+          <span className="main-label">{label}</span>
+          {subLabel && <span className="sub-label">{subLabel}</span>}
+        </label>
+        <input {...delegated} id={appliedId} />
+      </div>
+    </>
   );
 }
