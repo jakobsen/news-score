080d0832c11be08d4d80dd75731bdd3d526ae359
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2025-10-22 19:08:59 +0200

    Validate that the measurement list contains exactly one of each measurement

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,9 +1,9 @@
 from enum import StrEnum
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Self
 
 from litestar import Litestar, Request, post
 from litestar.plugins.structlog import StructlogPlugin
-from pydantic import BaseModel, Field, NonNegativeInt, conint
+from pydantic import BaseModel, Field, NonNegativeInt, conint, model_validator
 
 
 class MeasurementType(StrEnum):
@@ -13,17 +13,17 @@ class MeasurementType(StrEnum):
 
 
 class TemperatureMeasurement(BaseModel):
-    type: Literal[MeasurementType.TEMPERATURE]
+    type: Literal[MeasurementType.TEMPERATURE] = MeasurementType.TEMPERATURE
     value: conint(gt=31, le=42)
 
 
 class HeartRateMeasurement(BaseModel):
-    type: Literal[MeasurementType.HEART_RATE]
+    type: Literal[MeasurementType.HEART_RATE] = MeasurementType.HEART_RATE
     value: conint(gt=24, le=220)
 
 
 class RespiratoryRateMeasurement(BaseModel):
-    type: Literal[MeasurementType.RESPIRATORY_RATE]
+    type: Literal[MeasurementType.RESPIRATORY_RATE] = MeasurementType.RESPIRATORY_RATE
     value: conint(gt=3, le=60)
 
 
@@ -34,7 +34,30 @@ Measurement = Annotated[
 
 
 class MeasurementsList(BaseModel):
+    """
+    A list of measurements containing exactly one of each measurement type.
+
+    If a measurement type is missing, an unknown measurement type is present,
+    or the measurements list contains duplicate measurement types, validation
+    will fail.
+    """
+
     measurements: list[Measurement]
+
+    @model_validator(mode="after")
+    def validate_list(self) -> Self:
+        seen = set()
+        for measurement in self.measurements:
+            if measurement.type in seen:
+                raise ValueError(f"Duplicate measurement types {measurement.type}")
+            seen.add(measurement.type)
+
+        all_measurement_types = {t.value for t in MeasurementType}
+        if seen != all_measurement_types:
+            missing_types = all_measurement_types - seen
+            raise ValueError(f"Missing measurement types {', '.join(missing_types)}")
+
+        return self
 
 
 class Result(BaseModel):
---
+++ backend/tests/test_measurements.py
@@ -0,0 +1,41 @@
+import pytest
+from pydantic import ValidationError
+
+from app.app import (
+    HeartRateMeasurement,
+    MeasurementsList,
+    RespiratoryRateMeasurement,
+    TemperatureMeasurement,
+)
+
+
+def test_that_we_cannot_create_empty_measurement_list() -> None:
+    with pytest.raises(ValidationError):
+        MeasurementsList(measurements=[])
+
+
+def test_that_passing_incomplete_list_raises() -> None:
+    with pytest.raises(ValidationError):
+        MeasurementsList(measurements=[HeartRateMeasurement(value=60)])
+
+
+def test_that_passing_duplicates_raises() -> None:
+    with pytest.raises(ValidationError):
+        MeasurementsList(
+            measurements=[
+                HeartRateMeasurement(value=60),
+                TemperatureMeasurement(value=37),
+                RespiratoryRateMeasurement(value=8),
+                RespiratoryRateMeasurement(value=9),
+            ]
+        )
+
+
+def test_that_passing_valid_input_does_not_raise() -> None:
+    assert MeasurementsList(
+        measurements=[
+            HeartRateMeasurement(value=60),
+            TemperatureMeasurement(value=37),
+            RespiratoryRateMeasurement(value=8),
+        ]
+    )
